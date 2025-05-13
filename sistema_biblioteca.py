26771fb786b016052cf4e3334bccdffee3a93352
VictorManoel16 <vm20194546@gmail.com>
2025-05-13 12:30:52 -0300

    atualizando codigo,adicionando mais arquivos de texto

--- sistema_biblioteca.py
+++ sistema_biblioteca.py
@@ -1,11 +1,5 @@
-ARQUIVO_LIVROS = "livros.txt"
+import datetime
 
-def carregar_livros():
-    livros = []
-    try:
-        with open(ARQUIVO_LIVROS, "r", encoding="utf-8") as arquivo:
-            for linha in arquivo:
-                partes = linha.strip().split("|")
-                if len(partes) == 3:
-                    titulo, autor, status = partes
-                    livros.append({"titulo": titulo, "autor": autor, "status": status})
+ARQUIVO_LIVROS = "livros.txt"
+ARQUIVO_HISTORICO = "historico.txt"
+ARQUIVO_USUARIOS = "usuarios.txt"
